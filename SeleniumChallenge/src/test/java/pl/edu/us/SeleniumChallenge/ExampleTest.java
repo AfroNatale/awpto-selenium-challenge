3ffbfd71aa475896f5d8adf055f4d204d8e9138c
Artemiusz Znojek <AfroNatale@users.noreply.github.com>
2017-06-18 16:29:47 +0200

    Update ExampleTest.java

--- SeleniumChallenge/src/test/java/pl/edu/us/SeleniumChallenge/ExampleTest.java
+++ SeleniumChallenge/src/test/java/pl/edu/us/SeleniumChallenge/ExampleTest.java
@@ -2,16 +2,29 @@ package pl.edu.us.SeleniumChallenge;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.TakesScreenshot;
+
+
+
+
+
+
+
 
 public class ExampleTest {
 
@@ -19,7 +32,8 @@ public class ExampleTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+		driver = new ChromeDriver();
 	}
 
 	@After
@@ -51,5 +65,160 @@ public class ExampleTest {
         });
 		assertEquals("Instytut Informatyki", driver.getTitle());
 	}
+	
+	@Test
+	public void testCalc() {
+		
+		assertEquals(35, multiply(driver, 7,5 ));
+		SeleniumScreenshotHelper(driver, "D:\\test.jpg");
 
+	}
+	
+	
+	void SeleniumScreenshotHelper(WebDriver driver, String screenshotFileName){
+		File screenshot =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(screenshot, new File(screenshotFileName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	int multiply(WebDriver driver, int multiplier, int multiplicand) {
+	driver.get("http://www.anaesthetist.com/mnm/javascript/calc.htm");
+
+	String multiplierStr="";
+	String multiplicandStr="";
+	
+	switch(multiplicand){
+	case 0: {
+		multiplicandStr="zero";
+		break;
+		
+	}
+	case 1: {
+		multiplicandStr="one";
+		break;
+		
+	}
+	case 2: {
+		multiplicandStr="two";
+		break;
+		
+	}
+	case 3: {
+		multiplicandStr="three";
+		break;
+		
+	}
+	case 4: {
+		multiplicandStr="four";
+		break;
+		
+	}
+	case 5: {
+		multiplicandStr="five";
+		break;
+		
+	}
+	case 6: {
+		multiplicandStr="six";
+		break;
+		
+	}
+
+	case 7: {
+		multiplicandStr="seven";
+		break;
+		
+	}
+
+	case 8: {
+		multiplicandStr="eight";
+		break;
+		
+	}
+
+	case 9: {
+		multiplicandStr="nine";
+		break;
+		
+	}
+	}
+	
+	
+	
+	
+	switch(multiplier){
+	case 0: {
+		multiplierStr="zero";
+		break;
+		
+	}
+	case 1: {
+		multiplierStr="one";
+		break;
+		
+	}
+	case 2: {
+		multiplierStr="two";
+		break;
+		
+	}
+	case 3: {
+		multiplierStr="three";
+		break;
+		
+	}
+	case 4: {
+		multiplierStr="four";
+		break;
+		
+	}
+	case 5: {
+		multiplierStr="five";
+		break;
+		
+	}
+	case 6: {
+		multiplierStr="six";
+		break;
+		
+	}
+
+	case 7: {
+		multiplierStr="seven";
+		break;
+		
+	}
+
+	case 8: {
+		multiplierStr="eight";
+		break;
+		
+	}
+
+	case 9: {
+		multiplierStr="nine";
+		break;
+		
+	}
+	}
+	WebElement seven = driver.findElement(By.cssSelector("table [name='"+multiplierStr+"']"));
+	seven.click();
+	
+	WebElement multiplication = driver.findElement(By.cssSelector("table [name='mul']"));
+	multiplication.click();
+	
+	WebElement five = driver.findElement(By.cssSelector("table [name='"+multiplicandStr+"']"));
+	five.click();
+	
+	WebElement result = driver.findElement(By.cssSelector("table [name='result']"));
+	result.click();
+	
+	WebElement display = driver.findElement(By.cssSelector("table [name='Display']"));
+	return Integer.parseInt(display.getAttribute("value"));
+	}
 }
